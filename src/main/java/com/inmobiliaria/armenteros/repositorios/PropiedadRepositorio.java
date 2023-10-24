f2350cb426bdb74b3aec2ed8d4c7780a373c8021
Tonna7SA <gjbarrasa@gmail.com>
2023-10-24 18:21:56 -0300

    Arreglo precio minimo precio maximo 24/10 18:21

--- src/main/java/com/inmobiliaria/armenteros/repositorios/PropiedadRepositorio.java
+++ src/main/java/com/inmobiliaria/armenteros/repositorios/PropiedadRepositorio.java
@@ -18,13 +18,21 @@ public interface PropiedadRepositorio extends JpaRepository <Propiedad, Integer>
     @Query(value="SELECT * FROM Propiedad WHERE calle like %:calle%", nativeQuery = true) 
    public List<Propiedad> buscarPropiedadPorCalle(@Param("calle") String calle);
 
+//   @Query(value="select * from propiedad where (tipo_vivienda is null or tipo_vivienda like %:keyword%) "
+//           + "and (estado is null or estado like %:keyword1%) and (moneda is null or moneda like %:keyword2%) "
+//           + "and (localidad is null or localidad like %:keyword3%) "
+//           + "and (:keyword4 IS NULL OR :keyword4 = '' OR precio_propiedad IS NULL OR precio_propiedad <= :keyword4) "
+//           + "and (:keyword5 IS NULL OR :keyword5 = '' OR mts2totales IS NULL OR mts2totales <= :keyword5);", nativeQuery = true) 
+//   public List<Propiedad> buscarPropiedadPotTipoDeVivienda(@Param("keyword") String keyword, 
+//           @Param("keyword1") String keyword1, @Param("keyword2") String keyword2, 
+//           @Param("keyword3") String keyword3, @Param("keyword4") Long keyword4, @Param("keyword5") Long keyword5);
+   
    @Query(value="select * from propiedad where (tipo_vivienda is null or tipo_vivienda like %:keyword%) "
            + "and (estado is null or estado like %:keyword1%) and (moneda is null or moneda like %:keyword2%) "
            + "and (localidad is null or localidad like %:keyword3%) "
-           + "and (:keyword4 IS NULL OR :keyword4 = '' OR precio_propiedad IS NULL OR precio_propiedad <= :keyword4) "
-           + "and (:keyword5 IS NULL OR :keyword5 = '' OR mts2totales IS NULL OR mts2totales <= :keyword5);", nativeQuery = true) 
+           + "and (:keyword4 IS NULL OR :keyword4 = '' OR precio_propiedad IS NULL OR precio_propiedad >= :keyword4) "
+           + "and (:keyword5 IS NULL OR :keyword5 = '' OR precio_propiedad IS NULL OR precio_propiedad <= :keyword5);", nativeQuery = true) 
    public List<Propiedad> buscarPropiedadPotTipoDeVivienda(@Param("keyword") String keyword, 
            @Param("keyword1") String keyword1, @Param("keyword2") String keyword2, 
            @Param("keyword3") String keyword3, @Param("keyword4") Long keyword4, @Param("keyword5") Long keyword5);
-   
 }
